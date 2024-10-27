317201b782cde9e89fcb7e5783337a6638d55557
Gustavo Farias <gustavoburgerf@gmail.com>
2024-10-27 17:30:21 -0300

    Ajuste no dist

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,23 @@
 "use strict";
 
-console.log('O´la');
+var alunos = [{
+    nome: "Ana",
+    nota: 5
+}, {
+    nome: "Bernardo",
+    nota: 3
+}, {
+    nome: "Júlia",
+    nota: 7
+}, {
+    nome: "Gustavo",
+    nota: 9
+}, {
+    nome: "Rodrigo",
+    nota: 6
+}];
+var filtraAlunos = function filtraAlunos(aluno) {
+    return aluno.nota >= 6;
+};
+var notaDosAlunos = alunos.filter(filtraAlunos);
+console.log(notaDosAlunos);
